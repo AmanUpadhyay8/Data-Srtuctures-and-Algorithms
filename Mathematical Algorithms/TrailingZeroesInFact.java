acbc84c40dd626b0b33734e507aca75d6a631127
Aman Upadhyay <58130120+AmanUpadhyay8@users.noreply.github.com>
2021-09-14 20:23:23 +0530

    Added code for Trailing Zeroes in Factorials.

---
+++ Mathematical Algorithms/TrailingZeroesInFact.java
@@ -0,0 +1,12 @@
+public class TrailingZeroesInFact {
+    public static void main( Strings [] args) {
+
+        int n;
+        int res = 0;
+
+        for ( int i = 5 ; i <= n ; i = i * 5 ){
+            res = res + n/i;
+        }
+        System.out.println(res);
+    }
+}
